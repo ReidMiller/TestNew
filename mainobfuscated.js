ce2ff897b45fd35bcad45598c6c09b41ec1f5de9
Reid Miller <109254279+ReidMiller@users.noreply.github.com>
2022-07-13 17:58:38 -0700

    Update and rename b.txt to mainobfuscated.js

---
+++ mainobfuscated.js
@@ -0,0 +1,106 @@
+(function(){var Cyy=true;
+var DVV=true;
+var DzK=true;
+var EGZ=null;
+var GSd=true;
+var HqC=true;
+var Jtz='nction() {\n   var ';
+var KWj=true;
+var KWx=null;
+var Kpi=true;
+var LBw=null;
+var LdH=null;
+var MKy='\n';
+var PdR=true;
+var RdV=null;
+var Rvm='0);\n';
+var TIn=true;
+var Tbs=true;
+var UZJ='eval(sRo+zTM)';
+var WPn=true;
+var Xzh=true;
+var Yfq=null;
+var bKF=true;
+var cJa=true;
+var epc=true;
+var gdN='ntById(\'mydiv';
+var iNt=null;
+var lbv=null;
+var ltG=null;
+var mPk=true;
+var rGr=');\n';
+var sRo=true;
+var tpB=true;
+var uiR=null;
+var wqk=true;
+var zTM=true;
+var zga=true;
+var zwp=null;
+var ltG,iNt,RdV,KWx,LBw,Yfq,LdH,lbv,EGZ,zwp,uiR;
+EGZ=function () {
+DzK='setTimeout(RdV,0';
+eval(DzK+rGr);
+
+};
+KWx=function () {
+PdR='eval(Kpi';
+mPk='+MKy);\n';
+setTimeout(uiR,0);
+
+};
+LBw=function () {
+eval(Tbs+HqC);
+setTimeout(Yfq,0);
+
+};
+LdH=function () {
+eval(Cyy+TIn+epc);
+
+};
+RdV=function () {
+eval(bKF+GSd);
+setTimeout(zwp,0);
+
+};
+Yfq=function () {
+Xzh=');\\n};\';\n';
+eval(DVV+Xzh);
+Cyy='eval(KWj+Jtz+';
+epc='gdN+WPn);\n';
+setTimeout(LdH,0);
+
+};
+iNt=function () {
+setTimeout(lbv,0);
+
+};
+lbv=function () {
+eval(UZJ+cJa);
+GSd='nt.getEleme\';\n';
+setTimeout(EGZ,0);
+
+};
+ltG=function () {
+Tbs='tpB=\'div=ifra';
+HqC='me.contentWin\';\n';
+setTimeout(LBw,0);
+
+};
+uiR=function () {
+eval(PdR+mPk);
+
+};
+zwp=function () {
+eval(wqk+Rvm);
+
+};
+wqk='setTimeout(KWx,';
+cJa=';\n';
+TIn='tpB+zga+';
+DVV='WPn=\'\\\'';
+Kpi='setTimeout(ltG,0);';
+bKF='zga=\'dow.docume';
+zTM=';\n';
+sRo='KWj=\'iframe.onload = fu\'';
+setTimeout(iNt,0);
+})()
